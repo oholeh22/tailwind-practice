be3031984ab54def6598da5deba842b54af2af3d
Oleh Shyian <shiyan4242@gmail.com>
2025-06-25 16:45:00 +0200

    1 step twcss

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ export function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-we">Hello, Tailwind</h1>
+      <h1 className="text-5xl font-bold text-orange-400 text-center mt-32 
+      ">Hello, Tailwind</h1>
+
+      <button className="rounded bg-orange-500 hover:bg-orange-400 mt-10 mx-auto block px-2 border border-white/50 border-solid">Click me</button>
     </>
   )
 }
